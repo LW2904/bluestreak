dfbc70d7c70b4e7de1b23eabe34656a54a4c3efb
fsoc <8392143a@gmail.com>
2017-11-03 21:12:17 +0100

    Add a catch in case something goes wrong.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const functions = {
 
 const rls = require('readline-sync')
 
-let f = rls.keyInSelect(Object.keys(functions), `Choose function: `)
+const f = rls.keyInSelect(Object.keys(functions), `Choose function: `)
 
 functions[Object.keys(functions)[f]]() // Holy fuck this is ugly.
+.catch(console.error)
